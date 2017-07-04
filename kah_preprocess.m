b35b4978c1df70179b051c113ed28f9e33f4c3d1
tamtran10 <tammytran10@gmail.com>
2017-07-03 18:48:20 -0700

    Add ability to select channels using ft_channelselection

--- kah_preprocess.m
+++ kah_preprocess.m
@@ -3,9 +3,9 @@ clear
 
 % set the path to the data, and to the header/data/event files of a single subject
 info = kah_info;
-subject    = info.subj{2}; % change this to read in different datasets;  which subjects have which sessions/experiments are described in .../protocols/r1.json
+subject    = info.subj{15}; % change this to read in different datasets;  which subjects have which sessions/experiments are described in .../protocols/r1.json
 experiment = 'FR1';    % change this to read in different datasets;  only FR1/2 can be segmented by rmr_upennram_trialfun
-sessnum    = 1;      % change this to read in different datasets;  session numbers are zero-indexed
+sessnum    = 2;      % change this to read in different datasets;  session numbers are zero-indexed
 
 infocurr = info.(subject).(experiment).session(sessnum);
 
@@ -46,7 +46,14 @@ infocurr = info.(subject).(experiment).session(sessnum);
 
 %% Remove broken channels from data
 cfg = [];
-cfg.channel = setdiff(data.label, infocurr.badchan.broken);
+cfg.channel = setdiff(data.label, ft_channelselection(infocurr.badchan.broken, data.label));
+% cfg.channel = [ft_channelselection('RTS*', data.label); ft_channelselection('RTS*', data.label);
+
+% % cfg.channel = ft_channelselection({'all', '-RFG*', '-RIHG*', '-RFPS*'}, data.label);
+% cfg.channel = ft_channelselection({'RPT*'}, data.label);
+
+% datachannel = ft_preprocessing(cfg, data);
+
 data = ft_preprocessing(cfg, data);
 
 depthchan = nan(numel(data.label), 1);
@@ -61,18 +68,20 @@ for ichan = 1:numel(data.label)
     end
 end
 
+%% Visualize data
 cfg = []; % start with an empty cfg
 cfg.viewmode  = 'vertical'; % displays channels vertically, another option is a 'butterfly' plot
 cfg.ylim      = [-40 40];   % a choice of voltage limits to ensure each subject is seen from the same perspective (can be modified in the GUI)
 cfg.blocksize = 4;          % set the segment size to view at once to 4s
-cfg.channelcolormap = chancolors;
-cfg.colorgroups     = 1:numel(data.label);
+% cfg.channelcolormap = chancolors;
+% cfg.colorgroups     = 1:numel(data.label);
 ft_databrowser(cfg, data);   % opens a GUI for browsing through the data
 
 %% Detect line spectra
 params = struct;
-params.zthresh = .9; % default 2
-linespectra = rmr_findlinespectra(data.trial{1}, data.fsample, [50, data.fsample/2], params);
+params.zthresh = 5; % default 2
+% linespectra = rmr_findlinespectra(data.trial{1}, data.fsample, [50, data.fsample/2], params);
+linespectra = rmr_findlinespectra(data.trial{1}, data.fsample, [50, data.fsample/2 - 10], params);
 
 %% Add detected line spectra to kah_info.m and reload info
 info = kah_info;
@@ -90,18 +99,16 @@ cfg.bsfiltdir   = 'twopass';
     
 data = ft_preprocessing(cfg, data);
 
-%%
+%% Visualize data
 cfg = []; % start with an empty cfg
 cfg.viewmode  = 'vertical'; % displays channels vertically, another option is a 'butterfly' plot
 cfg.ylim      = [-40 40];   % a choice of voltage limits to ensure each subject is seen from the same perspective (can be modified in the GUI)
 cfg.blocksize = 4;          % set the segment size to view at once to 4s
 ft_databrowser(cfg, data);   % opens a GUI for browsing through the data
 
-
-
 %%
-[freq, medianpsd] = util_medianwelch(data.trial{1}(1,:), 2 * , 1600, 3200, data.fsample, []);
-
+[freq, medianpsd] = util_medianwelch(data.trial{1}(1,:), 2 * data.fsample, data.fsample/2, 2 * data.fsample, data.fsample, []);
+figure; loglog(freq, medianpsd)
 
 %% Detect line spectra
 peak = [60, 120, 180, 240, 300, 360, 420, 480];
