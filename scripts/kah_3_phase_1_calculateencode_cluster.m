453299220216a016f9949477ae21a5f90222032b
tamtran10 <tammytran10@gmail.com>
2017-11-09 21:09:23 -0800

    For cluster script, allowing skipping of already saved files/jobs

--- scripts/kah_3_phase_1_calculateencode_cluster.m
+++ scripts/kah_3_phase_1_calculateencode_cluster.m
@@ -4,8 +4,10 @@ clear; clc
 subjects = {'R1020J' 'R1032D' 'R1033D' 'R1034D' 'R1045E' 'R1059J' 'R1075J' 'R1080E' 'R1120E' 'R1135E' ...
     'R1142N' 'R1147P' 'R1149N' 'R1151E' 'R1154D' 'R1162N' 'R1166D' 'R1167M' 'R1175N'};
 clusterpath = '/projects/ps-voyteklab/tamtra/data/KAH/';
+clusterpath = '/Volumes/voyteklab/tamtra/data/KAH/';
 experiment = 'FR1';
 timewin = [-800, 1600];
+testtype = 'cmtest';
 
 % Set whether to permute trials or not.
 dopermute = 0;
@@ -22,12 +24,12 @@ end
 [datA, datB, encoding, type, outputfile] = deal({});
         
 % Set true if just testing one run
-testrun = 1;
+testrun = 0;
 
 % Change these params for non test runs.
-stack = 40; timreq = 90; memreq = 0.05 * 1024^3;
+stack = 40; timreq = 300; memreq = 0.05 * 1024^3;
 
-for isubj = 1
+for isubj = 2:length(subjects)
     % Get current subject identifier.
     subject = subjects{isubj};
 
@@ -48,22 +50,34 @@ for isubj = 1
     for iperm = 1:nperm
         % Specify inputs to kah_subfunc_phaseencode per channel pair. 
         for ipair = 1:nchanpair
+            % Determine name of output file.
+            if dopermute
+                newfile = [clusterpath subject '_FR1_phaseencode_' testtype '_' num2str(timewin(1)) '_' num2str(timewin(2)) '_pair_' num2str(ipair) '_resamp_' num2str(iperm) '.mat'];
+            else
+                newfile = [clusterpath subject '_FR1_phaseencode_' testtype '_' num2str(timewin(1)) '_' num2str(timewin(2)) '_pair_' num2str(ipair) '_nosamp.mat'];
+            end
+            
+            % Skip job if this channel pair has already been run.
+            if exist(newfile, 'file')
+                continue
+            end
+               
+            % Set inputs for kah_calculatephaseencode.m
             channums = chanpairs(ipair, :);
             datA = [datA; [clusterpath 'thetaphase/' subject '_' experiment '_thetaphase_' num2str(channums(1)) '.mat']];
             datB = [datB; [clusterpath 'thetaphase/' subject '_' experiment '_thetaphase_' num2str(channums(2)) '.mat']];
-            type = [type; 'cmtest'];
+            type = [type; testtype];
             
             if dopermute
                 encoding = [encoding; permtrial{isubj, iperm}];
-                outputfile = [outputfile; [clusterpath subject '_FR1_phaseencode_' type{ipair} '_' num2str(timewin(1)) '_' num2str(timewin(2)) '_pair_' num2str(ipair) '_resamp_' num2str(iperm) '.mat']];
             else
                 encoding = [encoding; {[]}];
-                outputfile = [outputfile; [clusterpath subject '_FR1_phaseencode_' type{ipair} '_' num2str(timewin(1)) '_' num2str(timewin(2)) '_pair_' num2str(ipair) '_nosamp.mat']];
             end
+            outputfile = [outputfile; newfile];
         end
     end
 end
-
+%%
 % Max out time and mem requests for test run.
 if testrun
     timreq = 28800; stack = 1; memreq = 4 * 1024^3;
