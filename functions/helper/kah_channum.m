21224045a746ebae5c5f7bd544ec9bdf2c80d93a
tamtran10 <tammytran10@gmail.com>
2017-11-09 17:39:55 -0800

    Adding number of surface channel output to kah_channum.m

--- functions/helper/kah_channum.m
+++ functions/helper/kah_channum.m
@@ -2,15 +2,16 @@ function channum = kah_channum(info, varargin)
 
 % KAH_CHANNUM prints the number of total and clean temporal and frontal surface channels per subject.
 % The first two columns are the total numbers of temporal and frontal channels.
-% The last two columns are the numbers of clean temporal and frontal channels.
+% The next two columns are the numbers of clean temporal and frontal channels.
+% The last column is the total number of clean surface channels.
 % Input is the info struct returned by KAH_INFO.
 %
 % Usage: 
 %   kah_channum(info) returns info for all subjects listed in info.subj
 %
-%   kah_channum(info, subject) returns info just the subject specified
+%   kah_channum(info, subjects) returns info just for the subjects specified
 
-if nargin == 0
+if nargin == 1
     subjoi = info.subj;
 else
     subjoi = varargin{1};
@@ -20,7 +21,7 @@ else
     end
 end
     
-channum = nan(length(subjoi), 4);
+channum = nan(length(subjoi), 5);
 
 for isubj = 1:length(subjoi)
     subject = subjoi{isubj};
@@ -32,4 +33,5 @@ for isubj = 1:length(subjoi)
     channum(isubj, 2) = sum(strcmpi('f', info.(subject).allchan.lobe(surface)));
     channum(isubj, 3) = sum(strcmpi('t', info.(subject).allchan.lobe(surface & clean)));
     channum(isubj, 4) = sum(strcmpi('f', info.(subject).allchan.lobe(surface & clean)));
+    channum(isubj, 5) = sum(surface & clean);
 end
