d61659e33fc43445f21675b3b1b306c7ab324e2d
tamtran10 <tammytran10@gmail.com>
2017-11-28 09:08:40 -0800

    Determine srate based on info struct, saving output using v7 so can use scipy to load file for fooof

--- scripts/kah_2_psd_0_calculatepsd.m
+++ scripts/kah_2_psd_0_calculatepsd.m
@@ -7,7 +7,7 @@ info = kah_info;
 clearvars('-except', 'info')
 
 % Set time window of interest.
-timewin = [-800, 1600];
+timewin = [-800, 0];
 
 for isubj = 1:length(info.subj)
     subject = info.subj{isubj};
@@ -18,15 +18,15 @@ for isubj = 1:length(info.subj)
     [dat, trialinfo, chans, times, temporal, frontal] = kah_loadftdata(info, subject, 'broadband', timewin, 1);
 
     % Construct PSDs.
-    nfft = length(times); % use the full trial
+    nfft = length(times); % use the full trial (no padding)
     nfreq = ceil(nfft/2) + 1;
 
     psds = nan(length(chans), nfreq, size(trialinfo, 1));
     for ichan = 1:length(chans)
-        [freq, psds(ichan,:,:)] = util_taperpsd(squeeze(dat(ichan,:,:)), srate, nfft, 'hanning');
+        [freq, psds(ichan,:,:)] = util_taperpsd(squeeze(dat(ichan,:,:)), info.(subject).fs, nfft, 'hanning');
     end
 
     % Save PSDs.
-    save([info.path.processed.hd subject '_FR1_psd_' num2str(timewin(1)) '_' num2str(timewin(2)) '.mat'], 'timewin', 'trialinfo', 'times', 'dat', 'freq', 'psds', 'chans', 'temporal', 'frontal')
+    save([info.path.processed.hd subject '_FR1_psd_' num2str(timewin(1)) '_' num2str(timewin(2)) '.mat'], 'timewin', 'trialinfo', 'times', 'dat', 'freq', 'psds', 'chans', 'temporal', 'frontal', '-v7')
 end
 disp('Done')
