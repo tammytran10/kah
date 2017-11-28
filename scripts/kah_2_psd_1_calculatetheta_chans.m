efc3bf687764a64f0075c83a20b2884c53013b5f
tamtran10 <tammytran10@gmail.com>
2017-11-28 12:22:25 -0800

    Adding calculation of amplitude in addition to band edges

--- scripts/kah_2_psd_1_calculatetheta_chans.m
+++ scripts/kah_2_psd_1_calculatetheta_chans.m
@@ -7,6 +7,9 @@ info = kah_info;
 %%
 clearvars('-except', 'info')
 
+% Choose time window.
+timewin = [-800, 0];
+
 % Choose to aggregate all theta peaks ('all') or just get the max peak ('max').
 pickpeak = 'all';
 
@@ -15,20 +18,21 @@ pickpeak = 'all';
 default = [4, 8];
 
 % For saving theta bands per subject.
-bands = cell(length(info.subj), 1);
+[bands, amplitudes] = deal(cell(length(info.subj), 1));
 
 for isubj = 1:length(info.subj)
     subject = info.subj{isubj};
     
     % Load FOOOF output for each subject.
-    load([info.path.processed.hd subject '_FR1_fooof_-800_1600.mat'], 'fooof')
+    load([info.path.processed.hd subject '_FR1_fooof_' num2str(timewin(1)) '_' num2str(timewin(2)) '.mat'], 'fooof')
 
     % For saving bands per channel.
     nchan = length(fooof);
     bands{isubj} = nan(nchan, 2);
+    amplitudes{isubj} = nan(nchan, 1);
     
     for ichan = 1:nchan
-        edges = [];
+        edges = []; amp = [];
         
         % Continue if no peaks were detected.
         if isempty(fooof{ichan})
@@ -49,18 +53,22 @@ for isubj = 1:length(info.subj)
             % Aggregate bandwidths.
             case 'all'
                 edges = [max(default), min(default)];
+                amp = 0;
                 for itheta = 1:size(theta, 1)
                     edges(1) = min([edges(1), theta(itheta, 1) - (theta(itheta, 3)/2)]);
                     edges(2) = max([edges(2), theta(itheta, 1) + (theta(itheta, 3)/2)]);
+                    amp = amp + theta(itheta, 2);
                 end
             % Use only the max peak.
             case 'max'
                 [~, argmax] = max(theta(:, 1));
                 edges(1) = theta(argmax, 1) - theta(argmax, 3)/2;
                 edges(2) = theta(argmax, 1) + theta(argmax, 3)/2;
+                amp = theta(argmax, 2);
         end
         bands{isubj}(ichan, :) = edges;
-    end
+        amplitudes{isubj}(ichan) = amp;
+    end 
 end
 
-save([info.path.processed.hd 'FR1_thetabands_-800_1600.mat'], 'bands')
+save([info.path.processed.hd 'FR1_thetabands_-800_1600.mat'], 'bands', 'amplitudes')
