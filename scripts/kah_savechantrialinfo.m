8f58cb0614996371d0924e88bc544beaf2525dd5
tamtran10 <tammytran10@gmail.com>
2017-12-11 15:14:59 -0800

    Adding script to save channel and trial info

---
+++ scripts/kah_savechantrialinfo.m
@@ -0,0 +1,38 @@
+clear
+
+info = kah_info;
+
+%%
+clearvars('-except', 'info')
+
+experiment = 'FR1';
+timewin = [0, 1600];
+
+[chans, pairs, chanregions, pairregions, encoding] = deal(cell(length(info.subj), 1));
+
+for isubj = 1:length(info.subj)
+    subject = info.subj{isubj};
+    disp([num2str(isubj) ' ' subject])
+    
+    input = load([info.path.processed.hd subject '_' experiment '_pac_between_ts_' num2str(timewin(1)) '_' num2str(timewin(2)) '_resamp.mat'], 'chanpairs', 'trialinfo', 'chans', 'temporal', 'frontal');
+    
+    chans{isubj} = 1:length(input.chans);
+    pairs{isubj} = input.chanpairs;
+    
+    chanregions{isubj} = cell(length(chans{isubj}), 1);
+    for ichan = 1:length(chans{isubj})
+        if input.temporal(ichan)
+            chanregions{isubj}{ichan} = 'T';
+        elseif input.frontal(ichan)
+            chanregions{isubj}{ichan} = 'F';
+        else
+            chanregions{isubj}{ichan} = 'N';
+        end
+    end
+    
+    pairregions{isubj} = chanregions{isubj}(pairs{isubj});
+    
+    encoding{isubj} = input.trialinfo(:, 3);
+end
+
+save([info.path.processed.hd 'FR1_chantrialinfo.mat'], 'chans', 'pairs', 'chanregions', 'pairregions', 'encoding')
