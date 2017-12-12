36deee588cea940261394ab67b69a5671bef42cd
tamtran10 <tammytran10@gmail.com>
2017-12-11 17:42:31 -0800

    Saving single-trial multi-channel data to CSV

--- scripts/kah_5_csv_0_savetocsv.m
+++ scripts/kah_5_csv_0_savetocsv.m
@@ -50,9 +50,12 @@ for isubj = 1:length(info.subj)
     
     for ipair = 1:nchan
         for itrial = 1:ntrial
+            % Build current line.
             linecurr = {info.subj{isubj}, info.age(isubj), ipair, chanregions{isubj}{ipair}, itrial, encoding{isubj}(itrial), ...
                 preslope{isubj}(ipair, itrial), postslope{isubj}(ipair, itrial), pretheta{isubj}(ipair, itrial), posttheta{isubj}(ipair, itrial), ...
                 hfabaseline{isubj}(ipair, itrial), hfaencoding{isubj}(ipair, itrial), tspac{isubj}.raw(ipair, itrial), tspac{isubj}.norm(ipair, itrial), tspac{isubj}.pvaltrial(ipair, itrial)};
+            
+            % Save current line.
             linecurr = cellfun(@string, linecurr, 'UniformOutput', false); % needs to be strings
             subjcurr(linenum, :) = linecurr;
             linenum = linenum + 1;
@@ -99,9 +102,12 @@ for isubj = 1:length(info.subj)
     subjcurr = cell(nchan, length(header));
     
     for ipair = 1:nchan
+        % Build current line.
         linecurr = {info.subj{isubj}, info.age(isubj), ipair, chanregions{isubj}{ipair}, ... 
             prethetapvals{isubj}(ipair), postthetapvals{isubj}(ipair), hfapval{isubj}(ipair)};
         linecurr = cellfun(@string, linecurr, 'UniformOutput', false); % needs to be strings
+        
+        % Save current line.
         subjcurr(ipair, :) = linecurr;
     end
     
@@ -145,6 +151,7 @@ for isubj = 1:length(info.subj)
     subjcurr = cell(npair, length(header));
     
     for ipair = 1:npair
+        % Build current line.
         linecurr = {info.subj{isubj}, info.age(isubj), pairs{isubj}(ipair, 1), pairs{isubj}(ipair, 2), ...
             pairregions{isubj}{ipair, 1}, pairregions{isubj}{ipair, 2}, ... 
             tspac{isubj}.AB.pvalpair(ipair), tspac{isubj}.BA.pvalpair(ipair), ... 
@@ -153,10 +160,13 @@ for isubj = 1:length(info.subj)
             phaseencoding{isubj}.onset(ipair), phaseencoding{isubj}.time(ipair), phaseencoding{isubj}.strength(ipair), phaseencoding{isubj}.nepisode(ipair)};
         linecurr = cellfun(@string, linecurr, 'UniformOutput', false); % needs to be strings
         
+        % Replace <missing> with NaNs.
         missing = cellfun(@ismissing, linecurr);        
         if sum(missing)
             linecurr(missing) = {num2str(nan)};
         end
+        
+        % Save current line.
         subjcurr(ipair, :) = linecurr;
     end
     
@@ -167,3 +177,54 @@ end
 
 % Save.
 util_cell2csv([info.path.csv 'kah_multichannel.csv'], csv, header)
+
+%% SINGLE-TRIAL, MULTI-CHANNEL
+clearvars('-except', 'info')
+
+% Load tsPAC.
+load([info.path.processed.hd 'FR1_tspac_between_0_1600.mat'], 'tspac');
+
+% Load channel and trial information.
+load([info.path.processed.hd 'FR1_chantrialinfo.mat'], 'pairs', 'pairregions', 'encoding')
+
+% Set names of metrics.
+header = {'subject', 'age', 'channelA', 'channelB', 'regionA', 'regionB', 'trial', 'encoding', ...
+    'rawtspacAB', 'rawtspacBA', 'normtspacAB', 'normtspacBA', 'pvaltspacAB', 'pvaltspacBA'};
+
+permission = []; % for first writing to disk. Data from previous runs will be cleared first.
+
+% Write to disk one line at a time because of the amount of data.
+for isubj = 1:length(info.subj)
+    disp(isubj)
+    npair = size(pairs{isubj}, 1);
+    ntrial = length(encoding{isubj});
+    
+    for ipair = 1:npair
+        for itrial = 1:ntrial
+            % Build current line.
+            linecurr = {info.subj{isubj}, info.age(isubj), pairs{isubj}(ipair, 1), pairs{isubj}(ipair, 2), ...
+                pairregions{isubj}{ipair, 1}, pairregions{isubj}{ipair, 2}, ... 
+                itrial, encoding{isubj}(itrial), ...
+                tspac{isubj}.AB.raw(ipair, itrial), tspac{isubj}.BA.raw(ipair, itrial), ... 
+                tspac{isubj}.AB.norm(ipair, itrial), tspac{isubj}.BA.norm(ipair, itrial), ... 
+                tspac{isubj}.AB.pvaltrial(ipair, itrial), tspac{isubj}.BA.pvaltrial(ipair, itrial), ... 
+                };
+            linecurr = cellfun(@string, linecurr, 'UniformOutput', false); % needs to be strings
+
+            % Replace <missing> with NaNs.
+            missing = cellfun(@ismissing, linecurr);        
+            if sum(missing)
+                linecurr(missing) = {num2str(nan)};
+            end
+            
+            % Switch to append and no header after one trial has been written to disk.
+            if itrial > 1
+                header = [];
+                permission = 'append';
+            end
+            
+            % Save current line right to disk.
+            util_cell2csv([info.path.csv 'kah_singletrial_multichannel.csv'], linecurr, header, permission)
+        end
+    end
+end
