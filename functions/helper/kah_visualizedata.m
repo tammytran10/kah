a1688b158a63bb8794c930526b35d254c22514b6
tamtran10 <tammytran10@gmail.com>
2018-01-10 15:09:28 -0800

    Returning data in addition to visualization

--- functions/helper/kah_visualizedata.m
+++ functions/helper/kah_visualizedata.m
@@ -1,7 +1,10 @@
-function kah_visualizedata(data)
+function data = kah_visualizedata(info, subject, datatype, timewin)
+% Load data
+data = kah_loadftdata(info, subject, datatype, timewin, 0);
+
 % Visualize data.
 cfg = []; 
 cfg.viewmode  = 'vertical';
 cfg.ylim      = [-40 40];  
-cfg.blocksize = length(data.times{1})/data.fsample;         
+cfg.blocksize = length(data.time{1})/data.fsample;         
 ft_databrowser(cfg, data);
