feb77a0a56e7a4309f3d4867d1d64d33fb317301
tamtran10 <tammytran10@gmail.com>
2018-02-12 13:16:25 -0800

    Adding feature scaling and saving coefficients to object

--- python/kah_classifier.py
+++ python/kah_classifier.py
@@ -9,15 +9,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
 
 class KahClassifier:
     """ Predict trial outcome (remembered vs. forgotten) using electrophysiological features before and during stimulus presentation 
     
     Parameters
     ----------
-    predictors : string or list of strings, optional
-        Electrophysiological features to use for classification. default: 'all'
-        Possible features include ['preslope', 'postslope', 'pretheta', 'posttheta', 'prehfa', 'posthfa', 'normtspac', normtspacmax']
+    predictors : list of tuples, optional
+        Electrophysiological features to use for classification. Each tuple should be ('measure', 'region'). default: 'all'
+    scoring : string, optional
+        The score to use for evaluating classifier performance. default: 'roc_auc'
+    cv : int, optional
+        The number of folds to use for cross-validation when testing multiple hyperparameters. default: 5
+    test_size : float, optional
+        The proportion of data to be held out to test final model performance. default: 0.3
+    seed : int, optional
+        Random state seed. default: 42
 
     Attributes
     ----------
@@ -39,7 +47,19 @@ class KahClassifier:
         self.seed = seed
     
     def classify(self, kahdata, method, hyperparameters=None):
-        """ Predict trial outcome using classifier type of interest. """
+        """ Predict trial outcome using classifier type of interest. 
+        
+        Parameters
+        ----------
+        kahdata : KahData() object
+            Data from subject(s) to classify
+        method : string
+            The type of classifier to use. Choices include 'logistic', 'SVM'
+        hyperparameters : dict, optional
+            Hyperparameters to try ({'C':[0.1, 1, 10, 100], 'kernel':['linear', 'rbf]}). 
+            Defaults determined by individual classifier functions.
+
+        """
 
         # Get predictor values and trial labels.
         self._set_predictors_labels(kahdata)
@@ -49,9 +69,22 @@ class KahClassifier:
         # The test set will be used to evaluate performance of a full model fit over the training set using the best hyperparameter.
         self.Xtrain, self.Xtest, self.ytrain, self.ytest = train_test_split(self.predvals, self.labels, test_size=self.test_size, shuffle=True, stratify=self.labels, random_state=self.seed)
 
+        # Scale features using the mean and variance of the training data.
+        self._standardscale_features()
+
         # For logistic regression.
         if method == 'logistic':
             self._logistic_regression(hyperparameters)
+
+    def _standardscale_features(self):
+        """ Scale features to have zero mean and unit variance. """
+
+        # Make scaler that stores mean and variance of the training data.
+        self.scaler = StandardScaler().fit(self.Xtrain)
+
+        # Scale training and test data using mean and variance of the training data.
+        self.Xtrain = self.scaler.transform(self.Xtrain)
+        self.Xtest = self.scaler.transform(self.Xtest)
 
     def _logistic_regression(self, hyperparameters):
         """ Classify using logistic regression. """
@@ -73,7 +106,7 @@ class KahClassifier:
         clf.fit(self.Xtrain, self.ytrain)
 
         if len(hyperparameters['C']) > 1:
-            self.cv_results_ = clf.cv_results_
+            self.grid_results_ = clf.cv_results_
         
         # Get probability of class labels (forgotten in column 0, forgotten in column 1) for the test set.
         self.proba_ = clf.predict_proba(self.Xtest)
@@ -81,10 +114,12 @@ class KahClassifier:
         # Calculate AUC of ROC curve for the test set.
         self.roc_auc_ = roc_auc_score(self.ytest, self.proba_[:, 1], average='weighted')
 
-        # Fit a model on all data, both training and test.
-        clf.fit(self.predvals, self.labels)
+        # Fit a model on all data, both training and test, re-scaled using training data.
+        clf.fit(self.scaler.transform(self.predvals), self.labels)
 
-        # TODO: save model coefficients to self
+        # Save model coefficients.
+        self.coef_ = clf.coef_
+        self.intercept_ = clf.intercept_
 
     def _set_predictors_labels(self, kahdata):
         """ Extract predictors of interest from KahData() object, aggregating across channels and channel pairs. 
@@ -100,16 +135,23 @@ class KahClassifier:
         stsc_ave = kahdata.stsc.pivot_table(index=['trial', 'region'], aggfunc = np.median).unstack()
         stmc_ave = kahdata.stmc.pivot_table(index=['trial', 'direction'], aggfunc = np.median).unstack()
 
+        # If all predictors are being used, get tuples of all the column names.
         if self.predictors == 'all':
             self.predictors = set(list(stsc_ave.columns) + list(stmc_ave.columns))
 
+        # Initialize data frame.
         self.predvals = pd.DataFrame()
+
+        # Concatenate each desired predictor column to the predvals data frame.
         for pred in self.predictors:
+
+            # Extract region and measure name.
             measure, region = pred
 
-            if len(region) > 1:
+            if len(region) > 1: # between-channel measures
                 self.predvals = pd.concat([self.predvals, stmc_ave[measure, region]], axis=1)
-            else:
+            else: # within-channel measures
                 self.predvals = pd.concat([self.predvals, stsc_ave[measure, region]], axis=1)
 
-        self.labels = np.array(stsc_ave['encoding'])[::2]
+        # Get labels for each trial (0 for forgotten, 1 for remembered).
+        self.labels = np.array(stsc_ave['encoding', 'F'])
