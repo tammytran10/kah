77af318e17ffb4796dd3bb01981eb458f5bc6f0f
tamtran10 <tammytran10@gmail.com>
2018-03-23 11:58:38 -0700

    Standardized to use dictionaries with same keys across subject types, filenames, data

--- python/kah_save_subject_data.py
+++ python/kah_save_subject_data.py
@@ -1,39 +1,35 @@
-# Script for loading Kahana data per subject and aggregating into temporal and frontal measures per trial.
+""" Script for loading Kahana data per subject and aggregating into temporal and frontal measures per trial. """
 
 from kah_data import SUBJECTS, KahData
 import pickle
 
-if __name__ == "__main__":
-    subjects_all = []
-    subjects_theta = []
-    subjects_theta_phase = []
+# All channels, only theta channels/pairs, only theta and phase encoding.
+SUBJECT_TYPES = ['all', 'theta', 'theta_phase']
 
+# Files to save to.
+SUBJECT_FILES = {}
+for subj_type in SUBJECT_TYPES:
+    SUBJECT_FILES[subj_type] = 'kah_subjects_{}.pickle'.format(subj_type)
+
+if __name__ == "__main__":
     # Load subject data.
+    subject_data = {}
+    for subj_type in SUBJECT_TYPES:
+        subject_data[subj_type] = []
+
     for isubj in range(len(SUBJECTS)):
         print(SUBJECTS[isubj])
         
         # For no channel exclusions.
-        subjects_all.append(KahData(subject=SUBJECTS[isubj], exclude_region='N', enforce_theta=False, enforce_phase=False, 
-                                theta_threshtype='bump'))
+        subject_data['all'].append(KahData(subject=SUBJECTS[isubj], exclude_region='N'))
 
         # For excluding channels without theta.
-        subjects_theta.append(KahData(subject=SUBJECTS[isubj], exclude_region='N', enforce_theta=True, enforce_phase=False, 
-                                theta_threshtype='bump'))
+        subject_data['theta'].append(KahData(subject=SUBJECTS[isubj], exclude_region='N', enforce_theta=True, theta_threshtype='bump'))
 
         # For excluding channels without theta and channel pairs without phase encoding.
-        subjects_theta_phase.append(KahData(subject=SUBJECTS[isubj], exclude_region='N', enforce_theta=True, enforce_phase=True, 
-                                theta_threshtype='bump'))
-
-    # For theta enforcement, drop subjects with low channel number per region.
-    subjects_theta_drop = ['R1033D', 'R1080E', 'R1120E']
-    subjects_theta = [subj for subj in subjects_theta if subj.subject not in subjects_theta_drop]
+        subject_data['theta_phase'].append(KahData(subject=SUBJECTS[isubj], exclude_region='N', enforce_theta=True, enforce_phase=True, theta_threshtype='bump'))
 
     # Save to disk.
-    with open('kah_subjects_all.pickle', 'wb') as file:
-        pickle.dump(subjects_all, file) 
-
-    with open('kah_subjects_theta.pickle', 'wb') as file:
-        pickle.dump(subjects_theta, file) 
-
-    with open('kah_subjects_theta_phase.pickle', 'wb') as file:
-        pickle.dump(subjects_theta_phase, file) 
+    for subj_type in SUBJECT_TYPES:
+        with open(SUBJECT_FILES[subj_type], 'wb') as file:
+            pickle.dump(subject_data[subj_type], file) 
