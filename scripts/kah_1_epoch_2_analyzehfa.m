47096693f11e321ab0e273d49cfcb198f708bc53
tamtran10 <tammytran10@gmail.com>
2018-04-04 09:10:33 -0700

    Removing playground code

--- scripts/kah_1_epoch_2_analyzehfa.m
+++ scripts/kah_1_epoch_2_analyzehfa.m
@@ -20,27 +20,3 @@ for isubj = 1:length(info.subj)
     end
 end
 save([info.path.processed.hd 'FR1_hfa.mat'], 'hfabaseline', 'hfaencoding', 'hfapval')
-
-%%
-figure; 
-for isubj = 1:length(info.subj)
-    subject = info.subj{isubj};
-    timewin = [-800, 1600];
-
-    [hfaamp, trialinfo, chans, times, temporal, frontal] = kah_loadftdata(info, subject, 'hfa', timewin, 1);
-
-    subplot(4, 5, isubj)
-    plot(times, mean(hfaamp, 3))
-end
-
-%%
-
-
-pvals = nan(length(chans), 1);
-for ichan = 1:length(chans)
-    baselinecurr = squeeze(mean(hfaamp(ichan, baseline(1):baseline(2), :), 2));
-    encodingcurr = squeeze(mean(hfaamp(ichan, toi(1):toi(2), :), 2));
-    pvals(ichan) = ranksum(encodingcurr, baselinecurr, 'tail', 'right');
-%     pvals(ichan) = signrank(stimcurr, 0, 'tail', 'right');
-end
-sum(pvals < 0.05/length(chans))
