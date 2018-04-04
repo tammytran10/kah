413907cfa033db3573ac925b417861019e9f3d24
tamtran10 <tammytran10@gmail.com>
2018-04-04 11:00:06 -0700

    Adding option to specify using individual or canonical theta phase

--- scripts/kah_4_pac_1_calculatetspac_cluster.m
+++ scripts/kah_4_pac_1_calculatetspac_cluster.m
@@ -13,6 +13,9 @@ if ~exist(clusterpath, 'dir')
     local = 1;
 end
 
+% Get individual or canonical theta phase data.
+thetalabel = 'cf';
+
 % Set experiment.
 experiment = 'FR1';
 
@@ -25,7 +28,7 @@ stack = 40; timreq = 300; memreq = 0.3 * 1024^3;
 % Pre-allocate input to qsubcellfun. Each cell element is one of the inputs for one job.
 % Each job is one channel pair.
 % qsubcellfun will parallelize across all channel pairs regardless of subject.
-[subjects, chanA, chanB, pairnums, paths] = deal({});
+[subjects, chanA, chanB, pairnums, paths, thetalabels] = deal({});
 
 totalpair = 0;
 
@@ -36,7 +39,7 @@ for isubj = 1:length(info.subj)
     disp([num2str(isubj) ' ' subject])
     
     % Get number of channels.
-    subjdata = matfile([clusterpath 'thetaphase/' subject '_FR1_thetaphase.mat']);
+    subjdata = matfile([clusterpath 'thetaphase/' subject '_FR1_thetaphase_' thetalabel '.mat']);
     nchan = length(subjdata.chans);
     
     % Get all unique pairs of channels.
@@ -63,11 +66,12 @@ for isubj = 1:length(info.subj)
         chanB = [chanB; channums(2)];
         pairnums = [pairnums; ipair];
         paths = [paths; clusterpath];
+        thetalabels = [thetalabels; thetalabel];
     end
     disp([num2str(sum(cellfun(@(x) ~isempty(x), strfind(subjects, subject)))) '/' num2str(nchanpair)])
 end
 disp([num2str(length(subjects)) '/' num2str(totalpair)])
-return
+
 % Max out time and mem requests for test run.
 if testrun
     timreq = 28800; stack = 1; memreq = 4 * 1024^3;
@@ -78,13 +82,13 @@ if local
     for irun = 1:length(subjects)
         tic
         memtic
-        kah_calculatepac(subjects{irun}, chanA{irun}, chanB{irun}, pairnums{irun}, paths{irun});
+        kah_calculatepac(subjects{irun}, chanA{irun}, chanB{irun}, pairnums{irun}, paths{irun}, thetalabels{irun});
         memtoc
         toc
     end
 else
     % Run me!
-    qsubcellfun('kah_calculatepac', subjects, chanA, chanB, pairnums, paths, ...
+    qsubcellfun('kah_calculatepac', subjects, chanA, chanB, pairnums, paths, thetalabels, ...
         'backend', 'torque', 'queue', 'hotel', 'timreq', timreq, 'stack', stack, 'matlabcmd', '/opt/matlab/2015a/bin/matlab', 'options', '-V -k oe ', 'sleep', 30, 'memreq', memreq)
 end
 disp('Done.')
