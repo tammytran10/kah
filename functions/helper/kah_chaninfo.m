10636e158795953497728696d54791dc49d7970b
tamtran10 <tammytran10@gmail.com>
2018-04-05 10:44:16 -0700

    Skipping channels in chanoi that are not in the contacts json

--- functions/helper/kah_chaninfo.m
+++ functions/helper/kah_chaninfo.m
@@ -12,7 +12,10 @@ end
 
 chaninfo = cell(length(chanoi), 7);
 for ichan = 1:length(chanoi)
-    chancurr = ismember(info.(subject).allchan.label, chanoi{ichan});
+    chancurr = ismember(info.(subject).allchan.label, upper(chanoi{ichan}));
+    if sum(chancurr) == 0
+        continue
+    end
     chaninfo(ichan,:) = [info.(subject).allchan.label(chancurr), info.(subject).allchan.type(chancurr), info.(subject).allchan.lobe(chancurr), info.(subject).allchan.altlobe(chancurr), info.(subject).allchan.ind.region(chancurr), info.(subject).allchan.mni.region(chancurr), info.(subject).allchan.tal.region(chancurr)];
 end
 chaninfo = cat(1, {'Label', 'Type', 'Lobe', 'Altlobe', 'Ind', 'MNI', 'TAL'}, chaninfo);
