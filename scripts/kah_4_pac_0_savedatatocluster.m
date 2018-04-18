8133721a8052c5ecabd4092a52b9a7e24aad1b2a
tamtran10 <tammytran10@gmail.com>
2018-04-18 14:01:59 -0700

    Adding script to reformat data to save onto cluster for PAC calculation

---
+++ scripts/kah_4_pac_0_savedatatocluster.m
@@ -0,0 +1,37 @@
+clear
+
+% Load Kahana info.
+info = kah_info;
+
+%%
+clearvars('-except', 'info')
+
+% Load canonical or individualized theta data.
+thetalabel = 'cf';
+
+% Set experiment.
+experiment = 'FR1';
+
+for isubj = 7 % 1:length(info.subj)    
+    % Get current subject identifier.
+    subject = info.subj{isubj};
+   
+    disp([num2str(isubj) ' ' subject])
+    
+    % Load subject theta phase, reformat, and save.
+    [data, trialinfo, chans, times, temporal, frontal] = kah_loadftdata(info, subject, ['thetaphase_' thetalabel], [-800, 1600], 1);
+    disp('Saving theta')
+    save(['/Volumes/voyteklab/tamtra/data/KAH/thetaphase/' subject '_FR1_thetaphase_' thetalabel '_-800_1600.mat'], 'data', 'trialinfo', 'chans', 'times')
+    
+    continue
+    
+    % Load subject HFA, reformat, and save.
+    [data, trialinfo, chans, times, temporal, frontal] = kah_loadftdata(info, subject, 'hfa', [-800, 1600], 1);
+    disp('Saving HFA')
+    save(['/Volumes/voyteklab/tamtra/data/KAH/hfa/' subject '_FR1_hfa_-800_1600.mat'], 'data', 'trialinfo', 'chans', 'times')
+    
+    % Copy trialshifts.
+    copyfile([info.path.processed.hd subject '/pac/ts/' subject '_' experiment '_pac_between_ts_trialshifts_default.mat'], ...
+        '/Volumes/voyteklab/tamtra/data/KAH/trialshifts/')
+end
+disp('Done.')
