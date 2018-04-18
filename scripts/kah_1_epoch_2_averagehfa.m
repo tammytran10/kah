40971d54fc0a052b3b01a51c079b7893797a0bc1
tamtran10 <tammytran10@gmail.com>
2018-04-18 13:58:09 -0700

    Adding late encoding HFA stage

--- scripts/kah_1_epoch_2_averagehfa.m
+++ scripts/kah_1_epoch_2_averagehfa.m
@@ -7,9 +7,10 @@ clearvars('-except', 'info')
 
 % Set windows for a pre-trial baseline period and an post-stimulus encoding period.
 baseline_time = [-800, 0];
-encoding_time = [0, 800];
+early_encoding_time = [0, 800];
+late_encoding_time = [800, 1600];
 
-[hfabaseline, hfaencoding, hfapval] = deal(cell(length(info.subj), 1));
+[hfa_baseline, hfa_early_encoding, hfa_late_encoding, hfa_pval] = deal(cell(length(info.subj), 1));
 
 for isubj = 1:length(info.subj)
     subject = info.subj{isubj};
@@ -20,26 +21,32 @@ for isubj = 1:length(info.subj)
 
     % Get indicies of time windows.
     baseline_ind = dsearchn(times(:), baseline_time(:)./1000);
-    encoding_ind = dsearchn(times(:), encoding_time(:)./1000);
+    early_encoding_ind = dsearchn(times(:), early_encoding_time(:)./1000);
+    late_encoding_ind = dsearchn(times(:), late_encoding_time(:)./1000);
 
     % Get average HFA across time windows.
-    hfabaseline{isubj} = squeeze(mean(hfaamp(:, baseline_ind(1):baseline_ind(2), :), 2));
-    hfaencoding{isubj} = squeeze(mean(hfaamp(:, encoding_ind(1):encoding_ind(2), :), 2));
+    hfa_baseline{isubj} = squeeze(mean(hfaamp(:, baseline_ind(1):baseline_ind(2), :), 2));
+    hfa_early_encoding{isubj} = squeeze(mean(hfaamp(:, early_encoding_ind(1):early_encoding_ind(2), :), 2));
+    hfa_late_encoding{isubj} = squeeze(mean(hfaamp(:, late_encoding_ind(1):late_encoding_ind(2), :), 2));
     
     % For each channel, determine if HFA during encoding is significantly higher than during baseline.
-    hfapval{isubj} = nan(length(chans), 1);
+    hfa_pval{isubj} = nan(length(chans), 1);
     for ichan = 1:length(chans)
-        baseline_chancurr = hfabaseline{isubj}(ichan, :);
-        encoding_chancurr = hfaencoding{isubj}(ichan, :);
-        hfapval{isubj}(ichan) = ranksum(encoding_chancurr, baseline_chancurr);
+        baseline_chancurr = hfa_baseline{isubj}(ichan, :);
+        encoding_chancurr = hfa_early_encoding{isubj}(ichan, :);
+        hfa_pval{isubj}(ichan) = ranksum(encoding_chancurr, baseline_chancurr);
     end
 end
 
-% Save baseline activity.
-hfa = hfabaseline;
-save([info.path.processed.hd 'FR1_hfa_' num2str(baseline_time(1)) '_' num2str(baseline_time(2)) '.mat'], 'hfa', 'hfapval')
+% % Save baseline activity.
+% hfa = hfa_baseline;
+% save([info.path.processed.hd 'FR1_hfa_' num2str(baseline_time(1)) '_' num2str(baseline_time(2)) '.mat'], 'hfa', 'hfapval')
+% 
+% % Save encoding activity.
+% hfa = hfa_early_encoding;
+% save([info.path.processed.hd 'FR1_hfa_' num2str(early_encoding_time(1)) '_' num2str(early_encoding_time(2)) '.mat'], 'hfa', 'hfapval')
 
 % Save encoding activity.
-hfa = hfaencoding;
-save([info.path.processed.hd 'FR1_hfa_' num2str(encoding_time(1)) '_' num2str(encoding_time(2)) '.mat'], 'hfa', 'hfapval')
+hfa = hfa_late_encoding;
+save([info.path.processed.hd 'FR1_hfa_' num2str(late_encoding_time(1)) '_' num2str(late_encoding_time(2)) '.mat'], 'hfa', 'hfa_pval')
 disp('Done.')
