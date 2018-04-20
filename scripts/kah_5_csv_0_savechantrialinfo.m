6244d100b3724f405ffb6096f2a6cb285e6ae5cf
tamtran10 <tammytran10@gmail.com>
2018-04-20 13:13:59 -0700

    Using info to find lobes instead of temporal and frontal arrays, also adding sublobes

--- scripts/kah_5_csv_0_savechantrialinfo.m
+++ scripts/kah_5_csv_0_savechantrialinfo.m
@@ -8,31 +8,32 @@ clearvars('-except', 'info')
 experiment = 'FR1';
 timewin = [0, 1600];
 
-[chans, pairs, chanregions, pairregions, encoding] = deal(cell(length(info.subj), 1));
+[chans, pairs, chanlobes, pairlobes, chanregions, pairregions, encoding] = deal(cell(length(info.subj), 1));
 
 for isubj = 1:length(info.subj)
     subject = info.subj{isubj};
     disp([num2str(isubj) ' ' subject])
     
-    input = load([info.path.processed.hd subject '/pac/ts/cf/' subject '_' experiment '_pac_between_ts_' num2str(timewin(1)) '_' num2str(timewin(2)) '_resamp.mat'], 'chanpairs', 'trialinfo', 'chans', 'temporal', 'frontal');
+    input = load([info.path.processed.hd subject '/pac/ts/cf/' subject '_' experiment '_pac_between_ts_' num2str(timewin(1)) '_' num2str(timewin(2)) '_resamp.mat'], 'chanpairs', 'trialinfo', 'chans');
     
     chans{isubj} = input.chans;
     pairs{isubj} = input.chanpairs;
     
-    chanregions{isubj} = cell(length(chans{isubj}), 1);
+    subjlobes = info.(subject).allchan.lobe;
+    subjregions = info.(subject).allchan.sublobe;
+    
+    [chanlobes{isubj}, chanregions{isubj}] = deal(cell(length(chans{isubj}), 1));
     for ichan = 1:length(chans{isubj})
-        if input.temporal(ichan)
-            chanregions{isubj}{ichan} = 'T';
-        elseif input.frontal(ichan)
-            chanregions{isubj}{ichan} = 'F';
-        else
-            chanregions{isubj}{ichan} = 'N';
-        end
+        chancurr = ismember(info.(subject).allchan.label, chans{isubj}{ichan});
+        
+        chanlobes{isubj}{ichan} = subjlobes{chancurr};
+        chanregions{isubj}{ichan} = subjregions{chancurr};
     end
     
+    pairlobes{isubj} = chanlobes{isubj}(pairs{isubj});
     pairregions{isubj} = chanregions{isubj}(pairs{isubj});
     pairs{isubj} = input.chans(input.chanpairs);
     encoding{isubj} = input.trialinfo(:, 3);
 end
 
-save([info.path.processed.hd 'FR1_chantrialinfo.mat'], 'chans', 'pairs', 'chanregions', 'pairregions', 'encoding')
+save([info.path.processed.hd 'FR1_chantrialinfo.mat'], 'chans', 'pairs', 'chanlobes', 'pairlobes', 'chanregions', 'pairregions', 'encoding')
