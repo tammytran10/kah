0587fffc3ea036be7da7fc754adb2dacac83b34c
tamtran10 <tammytran10@gmail.com>
2018-04-24 08:23:29 -0700

    Switching to cycle length threshold

--- scripts/kah_3_phase_1_savephaseepisodes.m
+++ scripts/kah_3_phase_1_savephaseepisodes.m
@@ -7,13 +7,13 @@ info = kah_info;
 clearvars('-except', 'info')
 
 testtype = 'corrcl';
-thetalabel = 'cf';
+thetalabel = 'canon';
 
 phaseencoding = cell(length(info.subj), 1);
 
 for isubj = 1:length(info.subj)
     % Extract episodes.
-    encoding = kah_getphaseencoding(info, info.subj{isubj}, testtype, 'time', 0.04, 'pvalue', [0, 1.5], 'all', thetalabel);
+    encoding = kah_getphaseencoding(info, info.subj{isubj}, testtype, 'cycle', 0.25, 'pvalue', [0, 1.6], 'all', thetalabel);
     
     % Save episode characteristics.
     phaseencoding{isubj}.nepisode = encoding.nepisode; % number of episodes in the time window
