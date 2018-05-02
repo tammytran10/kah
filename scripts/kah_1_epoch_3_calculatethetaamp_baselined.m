5ff7c5816e9b61d4048d40152e9a4bbac0b625a3
tamtran10 <tammytran10@gmail.com>
2018-05-02 12:33:31 -0700

    Saving early and late without baselining

--- scripts/kah_1_epoch_3_calculatethetaamp_baselined.m
+++ scripts/kah_1_epoch_3_calculatethetaamp_baselined.m
@@ -5,8 +5,8 @@ info = kah_info;
 
 %%
 experiment = 'FR1';
-timewins = {[0, 800], [800, 1600]};
-thetalabel = 'cf';
+timewins = {[-800, 0], [0, 800], [800, 1600]};
+thetalabel = 'canon';
 
 [thetaamp_all, times_all] = deal(cell(length(info.subj), 1));
 
@@ -25,13 +25,10 @@ for iwin = 1:length(timewins)
     thetaamp = cell(length(info.subj), 1);
     
     for isubj = 1:length(info.subj)
-        baselineind = dsearchn(times_all{isubj}(:), [-0.8; 0]);
-        baseline = squeeze(mean(thetaamp_all{isubj}(:, baselineind(1):baselineind(2), :), 2));
-       
         % Average theta amplitude.
         toi = dsearchn(times_all{isubj}(:), timewin(:)./1000);
-        thetaamp{isubj} = squeeze(mean(thetaamp_all{isubj}(:, toi(1):toi(2), :), 2)) - baseline;
+        thetaamp{isubj} = squeeze(mean(thetaamp_all{isubj}(:, toi(1):toi(2), :), 2));
     end
-    save([info.path.processed.hd 'FR1_thetaamp_' thetalabel '_' num2str(timewin(1)) '_' num2str(timewin(2)) '_baselined.mat'], 'thetaamp')
+    save([info.path.processed.hd 'FR1_thetaamp_' thetalabel '_' num2str(timewin(1)) '_' num2str(timewin(2)) '.mat'], 'thetaamp')
 end
 disp('Done.')
