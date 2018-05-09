bea61f42e529a23eb7678c127ea77a1f1b2e3238
tamtran10 <tammytran10@gmail.com>
2018-05-09 12:23:38 -0700

    Adding trialshifts for different windows

--- scripts/kah_4_pac_0_savedatatocluster.m
+++ scripts/kah_4_pac_0_savedatatocluster.m
@@ -12,7 +12,7 @@ thetalabel = 'cf';
 % Set experiment.
 experiment = 'FR1';
 
-for isubj = 7 % 1:length(info.subj)    
+for isubj = 1:length(info.subj)    
     % Get current subject identifier.
     subject = info.subj{isubj};
    
@@ -22,16 +22,18 @@ for isubj = 7 % 1:length(info.subj)
     [data, trialinfo, chans, times, temporal, frontal] = kah_loadftdata(info, subject, ['thetaphase_' thetalabel], [-800, 1600], 1);
     disp('Saving theta')
     save(['/Volumes/voyteklab/tamtra/data/KAH/thetaphase/' subject '_FR1_thetaphase_' thetalabel '_-800_1600.mat'], 'data', 'trialinfo', 'chans', 'times')
-    
-    continue
-    
+        
     % Load subject HFA, reformat, and save.
     [data, trialinfo, chans, times, temporal, frontal] = kah_loadftdata(info, subject, 'hfa', [-800, 1600], 1);
     disp('Saving HFA')
     save(['/Volumes/voyteklab/tamtra/data/KAH/hfa/' subject '_FR1_hfa_-800_1600.mat'], 'data', 'trialinfo', 'chans', 'times')
     
     % Copy trialshifts.
-    copyfile([info.path.processed.hd subject '/pac/ts/' subject '_' experiment '_pac_between_ts_trialshifts_default.mat'], ...
-        '/Volumes/voyteklab/tamtra/data/KAH/trialshifts/')
+    copyfile([info.path.processed.hd subject '/pac/ts/' subject '_' experiment '_pac_between_ts_trialshifts_default_-800_0.mat'], ...
+        '/Volumes/voyteklab/tamtra/data/KAH/shifttrials/')
+    copyfile([info.path.processed.hd subject '/pac/ts/' subject '_' experiment '_pac_between_ts_trialshifts_default_0_800.mat'], ...
+        '/Volumes/voyteklab/tamtra/data/KAH/shifttrials/')
+    copyfile([info.path.processed.hd subject '/pac/ts/' subject '_' experiment '_pac_between_ts_trialshifts_default_800_1600.mat'], ...
+        '/Volumes/voyteklab/tamtra/data/KAH/shifttrials/')
 end
 disp('Done.')
